f5fd9620c3d2c80c22021a706fc7e1de3b62b226
Angel Montes <angelmontes142005@gmail.com>
2025-09-21 09:56:52 -0600

    Crea funcion agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigos(){
+   let amigo = document.querySelector('#amigo').value;
+    if(amigos.includes(amigo)){
+            alert("Ese nombre ya fue agregado");
+        }
+        else{
+            amigos.push(amigo);
+        }
+    }
