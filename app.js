e34fb4abaf1af9ee499ab582087fdcf719aee097
Angel Montes <angelmontes142005@gmail.com>
2025-09-21 10:43:12 -0600

    Actualizar agregar amigo y agregar listado de amigos

--- app.js
+++ app.js
@@ -4,24 +4,32 @@ let amigos = [];
 function agregarAmigo(){
    let amigo = document.querySelector('#amigo').value;
     console.log(amigo);
-    console.log(amigos);
     if( document.querySelector('#amigo').value == ''){
         alert("Por favor, inserte un nombre");
     }
     else{
-        if(amigos.includes(amigo)){
-                alert("Ese nombre ya fue agregado");
+        if(!amigos.includes(amigo)){
+                amigos.push(amigo);
+                mostrarAmigos();
+                console.log(amigos);
             }
             else{
-                amigos.push(amigo);
+                alert("Ese nombre ya fue agregado");
             }
         }
         limpiarCaja();
     }
 
 
-agregarAmigo();
-
 function limpiarCaja(){
     document.querySelector('#amigo').value = '';
+}
+
+function mostrarAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
 }
